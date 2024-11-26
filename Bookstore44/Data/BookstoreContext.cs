7442058f9411566ce4f079c49f3ca72621f8f4ee
bruninho1507 <bruno.mendes15052007@gmail.com>
2024-11-26 14:58:23 -0300

    projeto att

---
+++ Bookstore44/Data/BookstoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bookstore.Models;
+using System.Collections.Generic;
+
+namespace Bookstore.Data
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
---
+++ Bookstore44/Services/Exceptions/IntegrityExeption.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookstore.Services.Exeptions
+{
+    public class IntegrityExeption : ApplicationException
+    {
+        public IntegrityExeption(string? message) : base(message)
+        {
+        }
+    }
+}
